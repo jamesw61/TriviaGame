5f7afd46d6c6afb79b8ae85b381554fa47fee8b8
James Wick <jmwick2@hotmail.com>
2017-05-10 00:49:01 -0700

    need to add next question timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,10 @@
-var questionList = ['Who authored the Monroe Doctrine?', "Who is buried in Grant's Tomb?", "What is two plus two?"];
-var correctAnswerList = ['John Quincy Adams', 'Nobody', 'four'];
-var allAnswerList = [ ['James Monroe', 'James Madison', 'John Quincy Adams', 'Marilyn Monroe'], ['Ulysses S. Grant', 'Julia Dent Grant', 'jefferson', 'grant'], ['one', 'two', 'three', 'four'] ];
-var imageList = [ "assets/images/monroe.jpg", "assets/images/grant.jpg", 'assets/images/two.jpg'];
+var questionList = ['Who authored the Monroe Doctrine?', "Who is buried in Grant's Tomb?", "Where did the Battle of Bunker Hill take place?"];
+var correctAnswerList = ['John Quincy Adams', 'Nobody', "Breed's Hill"];
+var allAnswerList = [ ['James Monroe', 'James Madison', 'John Quincy Adams', 'Marilyn Monroe'], ['Ulysses S. Grant', 'Julia Dent Grant', 'Mark Twain', 'Nobody'], ['Archie Hill', 'Bunker Hill', "Breed's Hill", 'Hamburger Hill'] ];
+var imageList = [ "assets/images/monroe.jpg", "assets/images/grant.jpg", "assets/images/bunker.jpg"];
 var qcount = 0;
 var timer;
 var rightAnswers = 0;
-
-
-
-activateMouseover();
-resetGame();
-
-$('#clickStart').on( "click", function() 
-	{
-		$('.panel-body').show();
-		askQuestion(qcount);
-		myCountDown();	
-	});
-
-$('#clickReset').on( "click", function()
-	{
-		resetGame();
-	})
 
 function resetGame(){
 	qcount = 0;
@@ -29,9 +12,8 @@ function resetGame(){
 	clearTimeout(timer);
 	$('#clickStart').show().html("Start");
 	$('.panel-body').not('#clickStart').hide();
-	$('#score').html("Wins:  " + rightAnswers);
+	$('#score').html("Correct Answers:  " + rightAnswers);
 }
-
 
 function askQuestion(qcount) {
 	
@@ -47,48 +29,9 @@ function askQuestion(qcount) {
 	isThisTheAnswer();
 }
 
-
-
-function isThisTheAnswer() {
-    var clickedAnswer;    
-    $('.answer').click(function() {
-    	clearTimeout(timer);
-        clickedAnswer = $(this).text();
-        $('.answer').off('click');
-        console.log(clickedAnswer);
-        console.log(correctAnswerList[qcount]);
-        checkAnswer();
-    	});
-
-    	function checkAnswer() {
-        	if (clickedAnswer == correctAnswerList[qcount]) {
-				rightAnswers++;
-        		qcount++;
-        		$('#score').html("Wins:  " + rightAnswers);
-        		$('.panel-body').hide();
-        		$('#clickStart').show().html("Correct - click for next question");
-        		haveAllQuestionsBeenAsked ();
-        	}      	
-        
-       		else {
-        	// $(this).text('Wrong');
-        	$('#clickStart').show().html("Wrong - click for next question");
-        	$('.answer').hide();
-        	$('#displayQuestion').hide();
-        	qcount++;
-        	haveAllQuestionsBeenAsked ();
-        	};
-
-        }
-    
-}
-
-
-//coundown function from stackoverflow
 function myCountDown(){
 	var n = 8;
 	countDown();
-	// timer = setTimeout(countDown,1000);
 	function countDown(){
   	 	n--;
    		if(n > 0){
@@ -99,7 +42,6 @@ function myCountDown(){
      		timeIsUp();
      	};
    	}
-   	
 }
 
 function timeIsUp() {
@@ -139,6 +81,73 @@ function haveAllQuestionsBeenAsked () {
 	if(qcount === questionList.length) {
 		$('#clickReset').show().html("All questions have been asked - Click Here To Reset");
 		$('#clickStart').hide();
+		$('.displayImage').hide();
 	}
 }
+
+function isThisTheAnswer() {
+    var clickedAnswer;    
+    $('.answer').click(function() {
+    	clearTimeout(timer);
+        clickedAnswer = $(this).text();
+        $('.answer').off('click');
+        console.log(clickedAnswer);
+        console.log(correctAnswerList[qcount]);
+        checkAnswer();
+    	});
+
+    	function checkAnswer() {
+        	if (clickedAnswer == correctAnswerList[qcount]) {
+				rightAnswers++;
+        		qcount++;
+        		$('#score').html("Wins:  " + rightAnswers);
+        		$('.panel-body').hide();
+        		$('#clickStart').show().html("Correct - click for next question");
+        		haveAllQuestionsBeenAsked ();
+        	}      	
+        
+       		else {
+        	// $(this).text('Wrong');
+        	$('#clickStart').show().html("Wrong - click for next question");
+        	$('.answer').hide();
+        	$('#displayQuestion').hide();
+        	qcount++;
+        	haveAllQuestionsBeenAsked ();
+        	};
+        }    
+}
+
+
+activateMouseover();
+resetGame();
+
+$('#clickStart').on( "click", function() 
+	{
+		$('.panel-body').show();
+		askQuestion(qcount);
+		myCountDown();	
+	});
+
+$('#clickReset').on( "click", function()
+	{
+		resetGame();
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
