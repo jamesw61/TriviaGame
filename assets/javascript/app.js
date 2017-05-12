554894ca4cff8fcc666982889050a732766cbd7b
James Wick <jmwick2@hotmail.com>
2017-05-12 03:10:57 -0700

    changed some fonts and spacing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
+$( document ).ready(function() {
+
 var questionList = ['Who authored the Monroe Doctrine?', 
 					"Who is buried in Grant's Tomb?", 
 					"Where did the Battle of Bunker Hill take place?", 
 					"The USS Constitution, also known as Old Ironsides, is a US Navy frigate made of what metal?"];
 var correctAnswerList = ['John Quincy Adams', 'Nobody', "Breed's Hill", "It's not metal, it's wood"];
 var allAnswerList = [ ['James Monroe', 'James Madison', 'John Quincy Adams', 'Marilyn Monroe'], 
-					['Ulysses S. Grant', 'Julia Dent Grant', 'Mark Twain', 'Nobody'], 
+					['Ulysses S. Grant', 'Julia Dent Grant', 'Hugh Grant', 'Nobody'], 
 					["Edith's Hill", 'Bunker Hill', "Breed's Hill", 'Hamburger Hill'], 
-					["Iron", "It's not metal, it's wood", "Titanium", "Steel"] ];
+					["Iron", "It's not metal, it's wood", "Titanium", "Parchment"] ];
 var imageList = [ "assets/images/monroe.jpg",
 				  "assets/images/grant.jpg width=345px height=451px", 
 				  "assets/images/bunker.jpg", 
@@ -28,24 +30,27 @@ function resetGame(){
 	clearTimeout(timer);
 	$('#clickStart').show().html("Start");
 	$('.panel-body').not('#clickStart').hide();
-	$('#score').html("Correct Answers:  " + rightAnswers + "<br><br>Incorrect Answers:  " + wrongAnswers);
+	$('#score').html("Correct Answers:  " + rightAnswers + "<br>Incorrect Answers:  " + wrongAnswers);
 }
 
 function askQuestion(qcount) {
 	clearTimeout(timerTwo);
 	$('.panel-body').show().css('background-color', 'black');
   	$('#displayQuestion').html(questionList[qcount]);
-	$('#answerOne').html(allAnswerList[qcount][0]);
-	$('#answerTwo').html(allAnswerList[qcount][1]);
-	$('#answerThree').html(allAnswerList[qcount][2]);
-	$('#answerFour').html(allAnswerList[qcount][3]);
+	// $('#answerOne').html(allAnswerList[qcount][0]);
+	// $('#answerTwo').html(allAnswerList[qcount][1]);
+	// $('#answerThree').html(allAnswerList[qcount][2]);
+	// $('#answerFour').html(allAnswerList[qcount][3]);
+	for (var j = 0; j < allAnswerList.length; j++) {
+		$('.answer').eq(j).html(allAnswerList[qcount][j]);
+	}
 	$('#clickStart').hide();
 	$('.nonanswer').hide();
 	$('.displayImage').show().html('<img src=' + imageList[qcount] + ' >');	
 }
 
 function questionCountDown(){
-	var n = 6;
+	var n = 8;
 	countDown();
 	function countDown(){
   	 	n--;
@@ -60,14 +65,13 @@ function questionCountDown(){
 }
 
 function timeIsUp() {
-	// qcount++;
 	$('.panel-body').hide();
     $('#clickStart').show().html("You ran out of time - click for next question");
     $('.result').show().html("The correct answer was: " + correctAnswerList[(qcount)]);
     $('.displayExp').html(explanations[(qcount)]).show();
-    timerTwo = setTimeout(nextQuestion,6000);
+    timerTwo = setTimeout(nextQuestion,8000);
     wrongAnswers++;
-    $('#score').html("Correct Answers:  " + rightAnswers + "<br><br>Incorrect Answers:  " + wrongAnswers);
+    $('#score').html("Correct Answers:  " + rightAnswers + "<br>Incorrect Answers:  " + wrongAnswers);
     qcount++;
     haveAllQuestionsBeenAsked ();
 }
@@ -98,15 +102,14 @@ function mouseoverById (id) {
 	});	
 }
 
-function haveAllQuestionsBeenAsked () {
-	
+function haveAllQuestionsBeenAsked () {	
 	if(qcount === questionList.length) {
 		clearTimeout(timerTwo);
 		$('#clickReset').show().html("All questions have been asked - Click Here To Reset");
 		$('#clickStart').hide();
 		$('.displayImage').hide();
 		$('.answer').hide();
-		$('#score').html("Correct Answers:  " + rightAnswers + "<br><br>Incorrect Answers:  " + wrongAnswers);
+		$('#score').html("Correct Answers:  " + rightAnswers + "<br>Incorrect Answers:  " + wrongAnswers);
 	}
 }
 
@@ -132,50 +135,45 @@ $('#clickReset').on( "click", function()
 
 $('.answer').on( "click", function()
 {
+	var clickedAnswer;
+    clearTimeout(timer);
+    clickedAnswer = $(this).text();
+   	
+    checkAnswer();
 
-		var clickedAnswer;
-		// console.log("b" + rightAnswers);
-    	clearTimeout(timer);
-        clickedAnswer = $(this).text();
-        // $('.answer').off('click');
-        // console.log("c" + rightAnswers);
-           
-    	
-        checkAnswer();
+    function checkAnswer() {
 
-        function checkAnswer() {
-
-    		$('.displayExp').html(explanations[(qcount)]).show();
+    	$('.displayExp').html(explanations[(qcount)]).show();
     		// console.log("d" + rightAnswers);
-        	if (clickedAnswer === correctAnswerList[(qcount)]) {
+        if (clickedAnswer === correctAnswerList[(qcount)]) {
         		// console.log("e" + rightAnswers);
-				rightAnswers++;
+			rightAnswers++;
 				// console.log("f" + rightAnswers);
-        		$('#score').html("Correct Answers:  " + rightAnswers + "<br><br>Incorrect Answers:  " + wrongAnswers);
-        		$('.panel-body').hide();
-        		timerTwo = setTimeout(nextQuestion,8000);
-        		$('#clickStart').show().html("Correct - click for next question");
-        		qcount++;
-        		haveAllQuestionsBeenAsked ();
-        	}      	
+        	$('#score').html("Correct Answers:  " + rightAnswers + "<br>Incorrect Answers:  " + wrongAnswers);
+        	$('.panel-body').hide();
+        	timerTwo = setTimeout(nextQuestion,8000);
+       		$('#clickStart').show().html("Correct - click for next question");
+       		qcount++;
+       		haveAllQuestionsBeenAsked ();
+        }      	
         
-       		else {
+       	else {
        		wrongAnswers++;
-        	$('#clickStart').show().html("Wrong - click for next question");
-        	$('#score').html("Correct Answers:  " + rightAnswers + "<br><br>Incorrect Answers:  " + wrongAnswers);
+       		$('#clickStart').show().html("Wrong - click for next question");
+       		$('#score').html("Correct Answers:  " + rightAnswers + "<br>Incorrect Answers:  " + wrongAnswers);
         	$('.answer').hide();
         	$('#displayQuestion').hide();
         	$('.result').show().html("The correct answer was: " + correctAnswerList[(qcount)]);
-        	timerTwo = setTimeout(nextQuestion,8000);
+       		timerTwo = setTimeout(nextQuestion,8000);
         	qcount++;
-        	haveAllQuestionsBeenAsked ();
-        	};
-        } 
+       		haveAllQuestionsBeenAsked ();
+        };
+    } 
 
-})
+});
 
 
-
+});
 	
 
 
